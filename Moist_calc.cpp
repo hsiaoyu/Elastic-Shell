a0db11f5813fe40f6f6b725aa4f02d35701a7418
hsiaoyu <blackkckk@gmail.com>
2017-02-12 01:05:31 -0600

    Moist_calc for uniform moisture level throughout the paper

---
+++ Moist_calc.cpp
@@ -0,0 +1,24 @@
+// This file generate the machine direction in barycentric coordinate of each face and store in MDinFace.dmat
+// Read out MDinFace.dmat using readDMAT and each row of the matrix is the barycentric coordinate representation of MD 
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <igl/readOBJ.h>
+#include <igl/readDMAT.h>
+#include <igl/writeDMAT.h>
+using namespace std;
+using namespace Eigen;
+MatrixXd V;// ENbar is the edge normal of Vbar
+MatrixXi F; // Eash row of EdgeNV(3*NTri,4) stores the index off the four vertices that are related to one edge
+int main (){
+    igl::readOBJ("V.obj", V, F);
+    VectorXd Moisture(F.rows());
+    double moist;
+    cout << "What is the uniform moisture difference(%):" << endl;
+    cin >> moist;
+    for(int i=0; i< F.rows(); i++){
+	Moisture(i)=moist;
+    }    
+    igl::writeDMAT("MoistureLevel.dmat",Moisture,1);
+    return 0;
+}
